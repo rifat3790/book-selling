ca1076a0ea745417d0837dbb6619c75c8d8451df
rifat3790 <mdrifayethossen@gmail.com>
2023-08-16 15:52:43 +0600

    all btn click complete

--- js/price.js
+++ js/price.js
@@ -1,3 +1,4 @@
+// Book 3
 document.getElementById('btn-1').addEventListener('click', function(){
     const book1Name = getTextElementString('book-1-name');
     
@@ -16,6 +17,7 @@ document.getElementById('btn-1').addEventListener('click', function(){
 
 })
 
+// Book 2 
 document.getElementById('btn-2').addEventListener('click', function(){
     const book2Name = getTextElementString('book-2-name');
     
@@ -30,6 +32,82 @@ document.getElementById('btn-2').addEventListener('click', function(){
     const showPrice = document.getElementById('total-price');
     const pp = document.createElement('p');
     pp.innerText = book2Price;
+    showPrice.appendChild(pp);
+
+})
+
+// Book 3
+document.getElementById('btn-3').addEventListener('click', function(){
+    const book3Name = getTextElementString('book-3-name');
+    
+    const book3Price = getTextElementNumber('book-3-price');
+    console.log(book3Name, book3Price);
+
+    const showName = document.getElementById('total-price');
+    const p = document.createElement('p');
+    p.innerText = book3Name;
+    showName.appendChild(p);
+
+    const showPrice = document.getElementById('total-price');
+    const pp = document.createElement('p');
+    pp.innerText = book3Price;
+    showPrice.appendChild(pp);
+
+})
+
+// Book 4
+document.getElementById('btn-4').addEventListener('click', function(){
+    const book4Name = getTextElementString('book-4-name');
+    
+    const book4Price = getTextElementNumber('book-4-price');
+    console.log(book4Name, book4Price);
+
+    const showName = document.getElementById('total-price');
+    const p = document.createElement('p');
+    p.innerText = book4Name;
+    showName.appendChild(p);
+
+    const showPrice = document.getElementById('total-price');
+    const pp = document.createElement('p');
+    pp.innerText = book4Price;
+    showPrice.appendChild(pp);
+
+})
+
+// Book 5
+document.getElementById('btn-5').addEventListener('click', function(){
+    const book5Name = getTextElementString('book-5-name');
+    
+    const book5Price = getTextElementNumber('book-5-price');
+    console.log(book5Name, book5Price);
+
+    const showName = document.getElementById('total-price');
+    const p = document.createElement('p');
+    p.innerText = book5Name;
+    showName.appendChild(p);
+
+    const showPrice = document.getElementById('total-price');
+    const pp = document.createElement('p');
+    pp.innerText = book5Price;
+    showPrice.appendChild(pp);
+
+})
+
+// Book 6
+document.getElementById('btn-6').addEventListener('click', function(){
+    const book6Name = getTextElementString('book-6-name');
+    
+    const book6Price = getTextElementNumber('book-6-price');
+    console.log(book6Name, book6Price);
+
+    const showName = document.getElementById('total-price');
+    const p = document.createElement('p');
+    p.innerText = book6Name;
+    showName.appendChild(p);
+
+    const showPrice = document.getElementById('total-price');
+    const pp = document.createElement('p');
+    pp.innerText = book6Price;
     showPrice.appendChild(pp);
 
 })
